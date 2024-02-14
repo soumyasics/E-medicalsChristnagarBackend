48f626e20f9c21028bc758ab7ca1fe5a8af9b276
RadhulSics <radhul.sics@gmail.com>
2024-02-14 15:59:19 +0530

    updated edit

--- User/userController.js
+++ User/userController.js
@@ -1,6 +1,5 @@
-const users=require('./userSchema')
-const multer=require('multer')
-
+const users = require("./userSchema");
+const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
@@ -12,213 +11,216 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage }).single("image");
-//User Registration 
+//User Registration
 
-const registerUser=(req,res)=>{
-
-
-    const newUser=new users({
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
-        housename:req.body.housename,
-        email:req.body.email,
-        city:req.body.city,
-        pincode:req.body.pincode,
-        contact:req.body.contact,
-        district:req.body.district,
-        password:req.body.password,
-        dob:req.body.dob,
-        gender:req.body.gender,
-        image:req.file
+const registerUser = (req, res) => {
+  const newUser = new users({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    housename: req.body.housename,
+    email: req.body.email,
+    city: req.body.city,
+    pincode: req.body.pincode,
+    contact: req.body.contact,
+    district: req.body.district,
+    password: req.body.password,
+    dob: req.body.dob,
+    gender: req.body.gender,
+    image: req.file,
+  });
+  newUser
+    .save()
+    .then((data) => {
+      res.json({
+        status: 200,
+        msg: "Inserted successfully",
+        data: data,
+      });
     })
-    newUser.save().then(data=>{
-        res.json({
-            status:200,
-            msg:"Inserted successfully",
-            data:data
-        })
-    }).catch(err=>{
-   
-        res.json({
-            status:500,
-            msg:"Data not Inserted",
-            Error:err
-        })
-    })
-}
+    .catch((err) => {
+      res.json({
+        status: 500,
+        msg: "Data not Inserted",
+        Error: err,
+      });
+    });
+};
 //User Registration -- finished
 
-//Login User 
-const loginUser=(req,res)=>{
-    const email=req.body.email
-    const password=req.body.password
-  
-    users.findOne({email:email}).exec().then(data=>{
-      if(password==data.password){
+//Login User
+const loginUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  users
+    .findOne({ email: email })
+    .exec()
+    .then((data) => {
+      if (password == data.password) {
         res.json({
-          status:200,
-          msg:"Login successfully",
-          data:data
-      })
-    }else{
-      res.json({
-        status:500,
-        msg:"password Mismatch",
-        
+          status: 200,
+          msg: "Login successfully",
+          data: data,
+        });
+      } else {
+        res.json({
+          status: 500,
+          msg: "password Mismatch",
+        });
+      }
     })
-    }
-    
-  }).catch(err=>{
-  res.json({
-      status:500,
-      msg:"User not found",
-      Error:err
-  })
-  })
-    };
-  
-  
-  //Login User --finished
-  
-  
-  //View all Users
-  
-  const viewUsers=(req,res)=>{
-    users.find().exec()
-    .then(data=>{
-      if(data.length>0){
+    .catch((err) => {
       res.json({
-          status:200,
-          msg:"Data obtained successfully",
-          data:data
-      })
-    }else{
-      res.json({
-        status:200,
-        msg:"No Data obtained "
-    })
-    }
-  }).catch(err=>{
-      res.json({
-          status:500,
-          msg:"Data not Inserted",
-          Error:err
-      })
-  })
-  
-  }
-  
-  // view Users finished
-  
-  
-  //update User by id
-  const editUserById=(req,res)=>{
-  
-    
-      
-    users.findByIdAndUpdate({_id:req.params.id},{
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
+        status: 500,
+        msg: "User not found",
+        Error: err,
+      });
+    });
+};
 
-        housename:req.body.housename,
-        email:req.body.email,
-        city:req.body.city,
-        pincode:req.body.pincode,
-        contact:req.body.contact,
-        district:req.body.district,
-        dob:req.body.dob
-      })
-  .exec().then(data=>{
-    res.json({
-        status:200,
-        msg:"Updated successfully"
+//Login User --finished
+
+//View all Users
+
+const viewUsers = (req, res) => {
+  users
+    .find()
+    .exec()
+    .then((data) => {
+      if (data.length > 0) {
+        res.json({
+          status: 200,
+          msg: "Data obtained successfully",
+          data: data,
+        });
+      } else {
+        res.json({
+          status: 200,
+          msg: "No Data obtained ",
+        });
+      }
     })
-  }).catch(err=>{
-    res.json({
-        status:500,
-        msg:"Data not Updated",
-        Error:err
+    .catch((err) => {
+      res.json({
+        status: 500,
+        msg: "Data not Inserted",
+        Error: err,
+      });
+    });
+};
+
+// view Users finished
+
+//update User by id
+const editUserById = (req, res) => {
+  users
+    .findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        housename: req.body.housename,
+        email: req.body.email,
+        city: req.body.city,
+        pincode: req.body.pincode,
+        contact: req.body.contact,
+        district: req.body.district,
+        dob: req.body.dob,
+        image:req.file
+      }
+    )
+    .exec()
+    .then((data) => {
+      res.json({
+        status: 200,
+        msg: "Updated successfully",
+      });
     })
-  })
-  }
+    .catch((err) => {
+      res.json({
+        status: 500,
+        msg: "Data not Updated",
+        Error: err,
+      });
+    });
+};
 // view cust by id
-  const viewUserById=(req,res)=>{
-    users.findOne({_id:req.params.id}).exec()
-    .then(data=>{
+const viewUserById = (req, res) => {
+  users
+    .findOne({ _id: req.params.id })
+    .exec()
+    .then((data) => {
       console.log(data);
       res.json({
-          status:200,
-          msg:"Data obtained successfully",
-          data:data
-      })
-    
-  }).catch(err=>{
-    console.log(err);
+        status: 200,
+        msg: "Data obtained successfully",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
       res.json({
-          status:500,
-          msg:"No Data obtained",
-          Error:err
-      })
-  })
-  
-  }
-  
-  const deleteUserById=(req,res)=>{
+        status: 500,
+        msg: "No Data obtained",
+        Error: err,
+      });
+    });
+};
 
-    users.findByIdAndDelete({_id:req.params.id}).exec()
-    .then(data=>{
+const deleteUserById = (req, res) => {
+  users
+    .findByIdAndDelete({ _id: req.params.id })
+    .exec()
+    .then((data) => {
       console.log(data);
       res.json({
-          status:200,
-          msg:"Data removed successfully",
-          data:data
-      })
-    
-  }).catch(err=>{
-    console.log(err);
+        status: 200,
+        msg: "Data removed successfully",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
       res.json({
-          status:500,
-          msg:"No Data obtained",
-          Error:err
-      })
-  })
-  
-  }
-  //forgotvPawd User by id
-  const forgotPwd=(req,res)=>{
-  
-    
-      
-    users.findOneAndUpdate({email:req.body.email},{
-     
-      password:req.body.password
-      })
-  .exec().then(data=>{
-    if(data!=null)
-    res.json({
-        status:200,
-        msg:"Updated successfully"
+        status: 500,
+        msg: "No Data obtained",
+        Error: err,
+      });
+    });
+};
+//forgotvPawd User by id
+const forgotPwd = (req, res) => {
+  users
+    .findOneAndUpdate(
+      { email: req.body.email },
+      {
+        password: req.body.password,
+      }
+    )
+    .exec()
+    .then((data) => {
+      if (data != null)
+        res.json({
+          status: 200,
+          msg: "Updated successfully",
+        });
+      else
+        res.json({
+          status: 500,
+          msg: "User Not Found",
+        });
     })
-    else
-    res.json({
-      status:500,
-      msg:"User Not Found"
-     
-  })
-  }).catch(err=>{
-    console.log(err);
-    res.json({
-        status:500,
-        msg:"Data not Updated",
-        Error:err
-    })
-  })
-  }
-  
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        status: 500,
+        msg: "Data not Updated",
+        Error: err,
+      });
+    });
+};
 
-
-
-module.exports={
+module.exports = {
   registerUser,
   viewUsers,
   editUserById,
@@ -226,5 +228,5 @@ module.exports={
   forgotPwd,
   viewUserById,
   deleteUserById,
-  upload
-}
+  upload,
+};
--- routes.js
+++ routes.js
@@ -6,21 +6,21 @@ const HospitalController=require('./Hospital/hospitalController')
 
 
 //user routes
-router.post('/registerUser',userController.upload,userController.registerUser)
-router.post('/loginUser',userController.loginUser)
-router.post('/viewUserById/:id',userController.viewUserById)
+router.post('/registerUser',userController.upload,userController.registerUser)//done
+router.post('/loginUser',userController.loginUser)//done
+router.post('/viewUserById/:id',userController.viewUserById)//done
 router.post('/viewUsers',userController.viewUsers)
-router.post('/editUserById/:id',userController.editUserById)
+router.post('/editUserById/:id',userController.upload,userController.editUserById)
 router.post('/deleteUserById/:id',userController.deleteUserById)
-router.post('/forgotPwdUser',userController.forgotPwd)
+router.post('/forgotPwdUser',userController.forgotPwd)//done
 
 
 //user routes
-router.post('/registerHospital',HospitalController.upload,HospitalController.registerHospital)
-router.post('/loginHospital',HospitalController.loginHospital)
-router.post('/viewHospitalById/:id',HospitalController.viewHospitalById)
+router.post('/registerHospital',HospitalController.upload,HospitalController.registerHospital)//done
+router.post('/loginHospital',HospitalController.loginHospital)//done
+router.post('/viewHospitalById/:id',HospitalController.viewHospitalById)//done
 router.post('/viewHospitals',HospitalController.viewHospitals)
-router.post('/editHospitalById/:id',HospitalController.editHospitalById)
+router.post('/editHospitalById/:id',HospitalController.upload,HospitalController.editHospitalById)
 router.post('/deleteHospitalById/:id',HospitalController.deleteHospitalById)
-router.post('/forgotPwdHospital',HospitalController.forgotPwd)
+router.post('/forgotPwdHospital',HospitalController.forgotPwd)//done
 module.exports=router
