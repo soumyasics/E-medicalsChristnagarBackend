698f396e0578349a95b2ffc2e7932debf5076469
RadhulSics <radhul.sics@gmail.com>
2024-02-17 15:38:19 +0530

    updated

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ router.post('/registerUser',userController.upload,userController.registerUser)//
 router.post('/loginUser',userController.loginUser)//done
 router.post('/viewUserById/:id',userController.viewUserById)//done
 router.post('/viewUsers',userController.viewUsers)
-router.post('/editUserById/:id',userController.upload,userController.editUserById)
+router.post('/editUserById/:id',userController.upload,userController.editUserById)//done
 router.post('/deleteUserById/:id',userController.deleteUserById)
 router.post('/forgotPwdUser',userController.forgotPwd)//done
 
@@ -21,16 +21,16 @@ router.post('/registerHospital',HospitalController.upload,HospitalController.reg
 router.post('/loginHospital',HospitalController.loginHospital)//done
 router.post('/viewHospitalById/:id',HospitalController.viewHospitalById)//done
 router.post('/viewHospitals',HospitalController.viewHospitals)
-router.post('/editHospitalById/:id',HospitalController.upload,HospitalController.editHospitalById)
+router.post('/editHospitalById/:id',HospitalController.upload,HospitalController.editHospitalById)//done
 router.post('/deleteHospitalById/:id',HospitalController.deleteHospitalById)
-router.post('/forgotPwdHospital',HospitalController.forgotPwd)
+router.post('/forgotPwdHospital',HospitalController.forgotPwd)//done
 
 //test
 router.post('/addTest',test.addTest)//done
 router.post('/deleteTestById/:id',test.deleteTestById)//done
 router.post('/viewTestById/:id',test.viewTestById)//done
 router.post('/editTestById/:id',test.ediTestById)//done
-router.post('/viewAllTests',test.viewTests)
+router.post('/viewAllTests',test.viewTests)//done
 router.post('/viewTestByLabId/:id',test.viewTestByLabId)
 
 
