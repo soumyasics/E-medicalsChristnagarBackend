b873fd701ef0daf0084c1a21ec2cc6c3b7cfc9a2
soumyasics <soumya.sics@gmail.com>
2024-02-26 23:12:17 +0530

    skr1

---
+++ Pharmacy/medicineSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const hSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    manufacturer: {
+        type: String,
+        required: true
+    },
+    description: String,
+    price: {
+        type: Number,
+        required: true
+    },
+    expiryDate: {
+        type: Date,
+        required: true
+    },
+    dosage: String,
+    comments: {
+        type: String,
+    },
+    img:Object,
+    count:{
+        type:Number,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('medicines', hSchema)
+
